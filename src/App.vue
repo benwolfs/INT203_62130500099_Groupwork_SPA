2f753a1787942696f233925c3934fd905143d14d
Supichaya <supichaya.jb@mail.kmutt.ac.th>
2021-04-17 04:18:22 +0700

    62130500099 [Add] base+App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="nav" class="h-12 bg-blue-800 bg-opacity-40 fixed w-screen z-10">
+    <div class="flex justify-end items-center text-white text-xl space-x-8 px-8 py-2 w-full font-serif">
+    <router-link to="/">Home</router-link>
+    <router-link to="/register">Register</router-link>
+    <router-link to="/member">Member</router-link>
+    </div>
   </div>
-  <router-view/>
+
+  <div class="w-full h-full bg-red-20 bg-no-repeat bg-center fixed" style="background-image: url(https://images8.alphacoders.com/632/thumb-1920-632051.png)"></div>
+  <router-view class="h-screen relative"/>
+
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+} */
 </style>
+
