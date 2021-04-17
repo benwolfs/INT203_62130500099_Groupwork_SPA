a150dae149a0fea3206b4ec5839faafc62c81ed8
Supichaya <supichaya.jb@mail.kmutt.ac.th>
2021-04-17 19:29:09 +0700

    62130500099[Add] router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Register from '../views/Register.vue'
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
+
   {
-    path: '/about',
-    name: 'About',
+    path: '/member',
+    name: 'Member',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Member.vue')
+  },
+  
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   }
 ]
 
